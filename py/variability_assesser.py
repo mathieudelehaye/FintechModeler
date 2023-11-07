b4f18fef7222d2182114494121b962adb70cbce9
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-11-07 07:50:29 +0000

    Split Python variability assesser single method
    So each split method can be called independently
    or replaced by another function, like one
    implemented in C/C++.

--- py/variability_assesser.py
+++ py/variability_assesser.py
@@ -31,51 +31,118 @@ class VariabilityAssesser:
     A class helping assess the variability of a stock price.
 
     Attributes:
-        attribute_1 (str): This is the description of attribute_1.
+        _stock_name (str): The name of the stock for which the 
+            price variability must be computed.
+        _stock_prices (array): An indexed array, which contains the 
+        stock price data. Read the array description below. 
     """
 
-    def plot_variability(self, stock_name, hide_plot=False):
+    def __init__(self, stock_name):
         """
-        Compute and plot the rolling variability, based on the Yahoo 
-        Finance data of the `pandas-datareader` library.
+        Constructor.
 
         Args:
-            stock_name (str): The name of the stock for which the price
-                variability must be plot.
+            stock_name (str): The name of the stock for which the 
+                price variability must be computed.
+        """
+        self._stock_name = stock_name
+        self._stock_prices = []
+
+    def get_stock_prices(self):
+        """
+        Getter for the stock prices.
+        
+        Returns:
+            An indexed array, which contains the stock price data. The row indices
+            are of `datetime` type and the column values are double (except when 
+            noted).
+            
+            Date (index)   Open      High       Low     Close  Adj Close      Volume [int]
+            ------------------------------------------------------------------------------
+            2010-01-04  7.622500  7.660714  7.585000  7.643214   6.487534   493729600
+            ...
+        """
+        return self._stock_prices
+
+    def get_variabilities(self):
+        """
+        Getter for the stock price variabilities.
+        
+        Returns:
+            An indexed array, which contains the stock price variabilities. The row indices
+            are of `datetime` type and the column values are double.
+            
+            Date (index)   Variability
+            --------------------------
+            2010-01-04         NaN
+            ...
+            2010-02-02      0.364328
+        """
+        return self._stock_prices['rolling_sigma']
+
+    def read_stock_prices(self, slice_size=0):
+        """
+        Read the underlying stock prices from the Yahoo Finance 
+        data of the `pandas-datareader` library.
+        Those prices will be used to compute the variability.
+
+        Args:
+            slice_size (optional, int): if greater than 0, sample a slice
+                of the `slice_size` first data rows, and replace the data 
+                array with it.
+        """
+        
+        # There is a type issue, due to a change in Yahoo Finance API. It needs
+        # a workaround: https://stackoverflow.com/questions/74832296
+        yf.pdr_override() 
+
+        symbols = [ self._stock_name ]
+        start_date = datetime(2010,1,1)    
+        end_date = datetime(2020,10,1) 
+        self._stock_prices = pdr.get_data_yahoo(symbols, start=start_date, end=end_date)
+        # print(self._stock_prices.head(10))
+
+        if (slice_size > 0):
+            # keep a slice with the first `slice_size` rows
+            end_index_number=slice_size-1
+            end_index_name=self._stock_prices.index[end_index_number]
+            self._stock_prices=self._stock_prices.loc[:end_index_name]
+        
+        # print(self._stock_prices)
+
+    def compute_variability(self):
+        """
+        Compute the rolling variability and add it to the stock price data.
+        """
+
+        self._stock_prices['change'] = self._stock_prices['Adj Close'].pct_change()
+        self._stock_prices['rolling_sigma'] = self._stock_prices['change'].rolling(20).std() * np.sqrt(255)
+
+        # print(f"mean: {self._stock_prices['rolling_sigma'].mean()}")
+        # print(f"std: {self._stock_prices['rolling_sigma'].std()}")
+        # print(self._stock_prices['rolling_sigma'].head(50))
+
+    def plot_variability(self):
+        """
+        Plot the rolling variability.
+        
+        Args:
             hide_plot (bool, optional): Set to True to hide the plot, 
                 e.g. for unit-testing the method. 
         
         Returns:
             int: This is the description of the return value.
         """
-
-        # There is a type issue, due to a change in Yahoo Finance API. It needs
-        # a workaround: https://stackoverflow.com/questions/74832296
-        yf.pdr_override() 
-
-        symbols = [ stock_name ]
-        start_date = datetime(2010,1,1)    
-        end_date = datetime(2020,10,1) 
-        data = pdr.get_data_yahoo(symbols, start=start_date, end=end_date)
-
-        print(data.head(10))
-
-        data['change'] = data['Adj Close'].pct_change()
-        data['rolling_sigma'] = data['change'].rolling(20).std() * np.sqrt(255)
-
-        print(f"mean: {data['rolling_sigma'].mean()}")
-        print(f"std: {data['rolling_sigma'].std()}")
-
-        if not hide_plot:
-            data['rolling_sigma'].plot()
+        
+        self._stock_prices['rolling_sigma'].plot()
             
-            plt.ylabel('$\sigma$')
-            plt.title('AAPL Stock Price Historical Volatility')
+        plt.ylabel('$\sigma$')
+        plt.title('AAPL Stock Price Historical Volatility')
 
-            # Connect the event handler to the click event
-            cid = plt.gcf().canvas.mpl_connect('button_press_event', self._on_plot_click)
+        # Connect the event handler to the click event
+        cid = plt.gcf().canvas.mpl_connect('button_press_event', self._on_plot_click)
 
-            plt.show()
+        plt.show()            
 
     def _on_plot_click(self, event):
         """

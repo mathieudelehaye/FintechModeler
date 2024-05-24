b0e73db44d24a6554fd0859cf0d3ba66b474c366
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2024-05-24 08:59:36 +0100

    Web architecture added to project
    Flask HelloWorld added.

---
+++ backend/controller/py/scripts/my_class.py
@@ -0,0 +1,10 @@
+# scripts/MyClass.py
+
+class MyClass:
+    def __init__(self):
+        # Constructor
+        pass
+
+    def my_method(self):
+        # Method definition
+        print("This is my method!")

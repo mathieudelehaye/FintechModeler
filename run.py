4c6f57a444502a13c0fe78becc2ecac99fd2c00c
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2024-05-26 10:42:23 +0100

    Average variability returned by webservice
    And calculated from data fetched from the Yahoo
    Finance REST API.
    Temporary route removed
    from webservice.

--- run.py
+++ run.py
@@ -11,18 +11,35 @@ def variability():
     end_month = request.args.get('end_month')
     stock_name = request.args.get('stock_name')
 
+    if not start_month:
+        start_month = 6
+    else:
+        # int required by read_stock_price
+        start_month = int(start_month)
+    
+    if not end_month:
+        end_month = 0
+    else:
+        # int required by read_stock_price
+        end_month = int(end_month)
+
+    if not stock_name:
+        stock_name = 'AAPL'
+
+    assesser = VariabilityAssesser(stock_name)
+    assesser.read_stock_price(start_month, end_month)
+
+    average_variability = assesser.compute_variability()
+
     data = {
         'start_month': start_month,
         'end_month': end_month,
-        'stock_name': stock_name
+        'stock_name': stock_name,
+        'averageVariability': average_variability
     }
 
     return jsonify(data)
 
-
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
     app.run(debug=True)

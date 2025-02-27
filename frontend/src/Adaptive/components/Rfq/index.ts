a58cdeabae0b287707159d3c73ec0b0f2e348e2f
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2025-02-27 10:27:35 +0000

    Duplicate copy of Adaptive library removed

--- frontend/src/Adaptive/components/Rfq/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  getRfqState$,
-  isRfq$,
-  onRejectQuote,
-  QuoteStateStage,
-  useRfqState,
-} from "./Rfq.state"
-export { RfqButton } from "./RfqButton"
-export { RfqTimer } from "./RfqTimer"
--- frontend/src/Adaptive/components/TearOut/handleTearOut.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export function handleTearOut(symbol: string, tileRef?: HTMLDivElement) {
-  return new Error("Function should be implemented at platform level")
-}

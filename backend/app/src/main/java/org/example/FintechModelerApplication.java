b9217e1de550ba2dc54c3c138ef16f3724217b58
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2025-05-28 13:51:54 +0100

    Backend replaced by HelloWorld SpringBoot project

---
+++ backend/app/src/main/java/org/example/FintechModelerApplication.java
@@ -0,0 +1,11 @@
+package com.example.fintechmodeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechModelerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FintechModelerApplication.class, args);
+  }
+}

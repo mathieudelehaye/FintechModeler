151954dfaf06b10fb56679255db3b479054fcf7f
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2025-07-13 00:18:04 +0100

    Backend URL updated

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:8080'
-    : 'https://backend20250103203956.azurewebsites.net'
+    : 'https://fintechmodeler-backend.wittyflower-c2822a5a.eastus.azurecontainerapps.io'
 };
 
 export default config;
